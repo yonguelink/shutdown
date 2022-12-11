c04b9df307c613d2fe113366b10296a8f1794d7f
yonguelink <isaac.computing@gmail.com>
2022-12-11 10:57:01 -0500

    Temporarily disable receiver off with broken receiver

--- src/shutdown.js
+++ src/shutdown.js
@@ -1,17 +1,17 @@
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
-const receiver = require('yamaha-receiver-control/src/request');
-const moment = require('moment');
+// const receiver = require('yamaha-receiver-control/src/request');
+// const moment = require('moment');
 
 module.exports = async function () {
   // Shutdown the receiver only if it's late - otherwise we might be working on something elsewhere with music
-  const now = moment();
-  const late = moment().hours(21).minute(00);
-  const wayTooEarly = moment().hours(5).minute(0);
+  // const now = moment();
+  // const late = moment().hours(21).minute(00);
+  // const wayTooEarly = moment().hours(5).minute(0);
 
-  if (now.isSameOrAfter(late) || now.isSameOrBefore(wayTooEarly)) {
-    await receiver.turnOff();
-  }
+  // if (now.isSameOrAfter(late) || now.isSameOrBefore(wayTooEarly)) {
+  //   await receiver.turnOff();
+  // }
 
   await exec('shutdown /s /t 0 /f');
 };
